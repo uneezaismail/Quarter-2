d1f470a05c57b04b7a82eb6fd47b3aa2de68ef34
uneezaismail <ismailrahet@gmail.com>
2024-10-07 10:51:12 +0500

    updated
    updated

--- assignment-4/app/components/cards/page.tsx
+++ assignment-4/app/components/cards/page.tsx
@@ -1,4 +1,4 @@
-interface Tprops {
+export interface Tprops {
   name: string;
   id: number;
   course: string;
@@ -6,11 +6,17 @@ interface Tprops {
   imageSrc: string;
 }
 
-const Card = (props: Tprops) => {
+const Card = ({ name, id, course, batch, imageSrc }: Tprops) => {
   return (
     <div>
-      <div className="border border-gray-300 rounded-3xl bg-white shadow-2xl xl:w-96 lg:w-80 sm:w-80 w-64 transform hover:scale-105 transition-transform duration-300">
-        <h2 className="flex justify-center sm:text-2xl text-lg p-2 font-bold bg-gray-900 text-white w-auto sm:mb-4 mb-2 rounded-t-3xl ">
+      <div
+        className="border border-gray-300 rounded-3xl bg-cover bg-center shadow-2xl xl:w-96 lg:w-80 sm:w-80 w-64 transform hover:scale-105 transition-transform duration-300"
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/free-vector/ombre-blue-curve-light-blue-background-vector_53876-140344.jpg')",
+        }}
+      >
+        <h2 className="flex justify-center sm:text-2xl text-lg p-2 font-bold bg-gr-900 text-blue-950 w-auto sm:mb-4 mb-2 rounded-t-3xl ">
           UNIVERSITY OF OXFORD
         </h2>
         <div className="flex justify-center">
@@ -22,30 +28,28 @@ const Card = (props: Tprops) => {
         <div className="flex justify-center sm:my-4 my-2 sm:h-44 h-32">
           <img
             className="sm:w-36 w-24 h-full  border-black sm:border-4 border-2"
-            src={props.imageSrc}
+            src={imageSrc}
             alt="Student-img"
           />
         </div>
 
-        
-          <div className="flex justify-center mb-4">
-            <div className="flex flex-col mr-4">
-              <p className="font-semibold sm:mb-2 sm:text-lg">NAME:</p>
-              <p className="font-semibold sm:mb-2 text-md sm:text-lg">ID NO:</p>
-              <p className="font-semibold sm:mb-2 text-md sm:text-lg">COURSE:</p>
-              <p className="font-semibold sm:mb-2 text-md sm:text-lg">BATCH:</p>
-            </div>
-
-            <div className="flex flex-col text-right">
-              <p className="sm:mb-2 text-md sm:text-lg">{props.name}</p>
-              <p className="sm:mb-2 text-md sm:text-lg">{props.id}</p>
-              <p className="sm:mb-2 text-md sm:text-lg">{props.course}</p>
-              <p className="sm:mb-2 text-md sm:text-lg">{props.batch}</p>
-            </div>
+        <div className="flex justify-center mb-4">
+          <div className="flex flex-col mr-4">
+            <p className="font-semibold sm:mb-2 sm:text-lg">NAME:</p>
+            <p className="font-semibold sm:mb-2 text-md sm:text-lg">ID NO:</p>
+            <p className="font-semibold sm:mb-2 text-md sm:text-lg">COURSE:</p>
+            <p className="font-semibold sm:mb-2 text-md sm:text-lg">BATCH:</p>
           </div>
-          
-        <div className="border-t-4 border-gray-700"></div>
-        <div className="bg-blue-600 sm:h-20 h-12 flex flex-col justify-end py-2 rounded-b-3xl p-4 text-white">
+
+          <div className="flex flex-col text-right">
+            <p className="sm:mb-2 text-md sm:text-lg">{name}</p>
+            <p className="sm:mb-2 text-md sm:text-lg">{id}</p>
+            <p className="sm:mb-2 text-md sm:text-lg">{course}</p>
+            <p className="sm:mb-2 text-md sm:text-lg">{batch}</p>
+          </div>
+        </div>
+
+        <div className="b-blue-600 sm:h-20 h-12 flex flex-col justify-end py-2 rounded-b-3xl p-4 text-white">
           <div className="flex justify-between sm:text-sm text-[10px]">
             <span className="overline">Authorized Signature</span>
             <span className="overline">Holder Signature</span>
--- assignment-4/app/page.tsx
+++ assignment-4/app/page.tsx
@@ -1,31 +1,45 @@
 import Card from "./components/cards/page";
+import { Tprops } from "./components/cards/page";
+
+let Students: Tprops[] =[{
+  name:"Haya",
+          id:1001,
+          course:"C++",
+          batch:"2024",
+          imageSrc:"https://img.freepik.com/premium-photo/muslim-hijab-girl_1001626-827.jpg"
+},
+{
+  name:"Imama",
+  id:1002,
+  course:"PHP",
+  batch:"2023",
+  imageSrc:"https://r2.starryai.com/results/1048598631/344ca507-130b-4d64-b0ef-dfb025116717.webp",
+},
+{
+  name:"Zumar",
+  id:1003,
+  course:"LLB",
+  batch:"2025",
+  imageSrc:"https://img.freepik.com/premium-photo/beautiful-muslim-girl-with-hijab_951949-49.jpg",
+}
+] 
 
 export default function Home() {
   return (
-    <div className="flex justify-center min-h-screen bg-orange-100">
+    <div className="flex justify-center min-h-screen bg-blue-100">
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-16 xl:p-20 md:p-15 py-8">
-        <Card
-          name="Haya"
-          id={1001}
-          course="C++"
-          batch="2024"
-          imageSrc="https://img.freepik.com/premium-photo/muslim-hijab-girl_1001626-827.jpg"
-        />
-        <Card
-          name="Imama"
-          id={1002}
-          course="PHP"
-          batch="2023"
-          imageSrc="https://r2.starryai.com/results/1048598631/344ca507-130b-4d64-b0ef-dfb025116717.webp"
-        />
-        <Card
-          name="Zumar"
-          id={1003}
-          course="LLB"
-          batch="2025"
-          imageSrc="https://img.freepik.com/premium-photo/beautiful-muslim-girl-with-hijab_951949-49.jpg"
-        />
+        {Students.map((student, index) => (
+          <Card
+            key={index}
+            name={student.name}
+            id={student.id}
+            course={student.course}
+            batch={student.batch}
+            imageSrc={student.imageSrc}
+          />
+        ))}
       </div>
     </div>
   );
 }
+  
